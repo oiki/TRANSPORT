5e68310df500355e24e76cb80995a0f23a53555c
Robocoders <team@blackbox.ai>
2024-10-07 10:14:46 +0000

    Added create action to TransportsController

---
+++ app/controllers/transports_controller.rb
@@ -0,0 +1,17 @@
+class TransportsController < ApplicationController
+  def create
+    @transport = Transport.new(transport_params)
+    @transport.requested_by = current_user.id
+    if @transport.save
+      redirect_to soignant_index_path, notice: 'Transport request was successfully created.'
+    else
+      render 'soignant/index'
+    end
+  end
+
+  private
+
+  def transport_params
+    params.require(:transport).permit(:description, :status, :assigned_to)
+  end
+end
